e42fdde604dc1cb84b297503418341b63888882e
pablort99 <pablort8599@gmail.com>
2021-11-05 18:19:43 +0100

    comentario

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -6,7 +6,7 @@ namespace HolaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo!");
+            Console.WriteLine("modificacion");
         }
     }
 }
